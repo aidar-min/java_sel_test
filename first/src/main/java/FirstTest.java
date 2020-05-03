e8dd2a1e049e33cf040feb7b7fe630c0fe0be05b
aidar-min <63909771+aidar-min@users.noreply.github.com>
2020-05-04 00:14:59 +0500

    Created a new project SuperTank

--- first/src/main/java/FirstTest.java
+++ first/src/main/java/FirstTest.java
@@ -1,6 +1,21 @@
+import test.tank.SuperTank;
+
 public class FirstTest {
-	
-	public static void main (String[] args) {
-		System.out.println ("We are here! Hello! ");
-	}
-}
+
+    public static void main(String[] args) {
+        //создаю экземпляр класса танка
+        SuperTank tank = new SuperTank();
+
+        //задаю конкретному танку конкретное свойство
+        tank.tankHeight = 253.25;
+        displayHeigh(tank);
+    }
+
+    public static void displayHeigh(SuperTank tank) {
+        System.out.println("Высота танка составляет : " + getSuperTankHeight(tank));
+    }
+
+        public static double getSuperTankHeight (SuperTank tank){
+            return tank.tankHeight;
+        }
+    }
---
+++ first/src/main/java/test/tank/SuperTank.java
@@ -0,0 +1,8 @@
+package test.tank;
+/*Created by Aydar 05 2020*/
+
+public class SuperTank {
+    /*высота*/
+    public double tankHeight;
+
+}
